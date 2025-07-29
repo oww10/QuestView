7f700a10093b319d44dd0bb73b22710f34814b52
gihung <52909865+gihung@users.noreply.github.com>
2025-07-29 15:35:11 +0900

    Initial Commit

---
+++ QuestView/QuestViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestViewApp.swift
+//  QuestView
+//
+//  Created by s s on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct QuestViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuestViewTests/QuestViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuestViewTests.swift
+//  QuestViewTests
+//
+//  Created by s s on 7/29/25.
+//
+
+import Testing
+@testable import QuestView
+
+struct QuestViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
